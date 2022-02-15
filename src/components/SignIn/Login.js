c7508a099fb5de723538dd8f94727a1097f83009
Abhinav <thakurabhinav17122001@gmail.com>
2022-02-15 17:04:17 +0530

    :

--- src/components/SignIn/Login.js
+++ src/components/SignIn/Login.js
@@ -5,32 +5,32 @@ import LoginCard from "./LoginCard";
 import SignUpCard from "./SignUPCard";
 
 const Login = (props) => {
-  const [signUp, setSignUp] = useState(false);
-  const signUpHandler = (value) => {
-    setSignUp(value);
-  };
+	const [signUp, setSignUp] = useState(false);
+	const signUpHandler = (value) => {
+		setSignUp(value);
+	};
 
-  return (
-    <div className={styles.loginScreen}>
-      <div className={styles.loginCardDiv}>
-        <h1 className={styles.trello}>Trello</h1>
-        {signUp ? (
-          <SignUpCard
-            signUpHandler={signUpHandler}
-            loginStateHandler={props.loginStateHandler}
-          />
-        ) : (
-          <LoginCard
-            signUpHandler={signUpHandler}
-            loginStateHandler={props.loginStateHandler}
-          />
-        )}
-      </div>
-      <div className={styles.lottieDiv}>
-        <img src={lottie} width="100%" height="100%"></img>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.loginScreen}>
+			<div className={styles.loginCardDiv}>
+				<h1 className={styles.trello}>Trello</h1>
+				{signUp ? (
+					<SignUpCard
+						signUpHandler={signUpHandler}
+						loginStateHandler={props.loginStateHandler}
+					/>
+				) : (
+					<LoginCard
+						signUpHandler={signUpHandler}
+						loginStateHandler={props.loginStateHandler}
+					/>
+				)}
+			</div>
+			<div className={styles.lottieDiv}>
+				<img src={lottie} width="100%" height="100%" alt="GIF"></img>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- src/components/SignIn/SignUPCard.js
+++ src/components/SignIn/SignUPCard.js
@@ -2,48 +2,48 @@ import React, { useState } from "react";
 import styles from "./LoginCard.module.css";
 
 const SignUpCard = (props) => {
-  const [signUpDetails, setSignUpDetails] = useState({
-    mail: "",
-    password: "",
-  });
+	const [signUpDetails] = useState({
+		mail: "",
+		password: "",
+	});
 
-  // setSignUpDetailshandler=()=>{
+	// setSignUpDetailshandler=()=>{
 
-  // }
-  return (
-    <div className={styles.loginCard}>
-      <input
-        type="email"
-        value={signUpDetails.mail}
-        placeholder="Enter mail"
-      ></input>
-      <input
-        type="password"
-        value={signUpDetails.password}
-        placeholder="Enter password"
-      ></input>
-      <input
-        type="password"
-        value={signUpDetails.password}
-        placeholder="Confirm password"
-      ></input>
-      <button
-        className={styles.loginButton}
-        onClick={() => {
-          props.loginStateHandler(true);
-        }}
-      >
-        Sign Up
-      </button>
-      <p
-        onClick={() => {
-          props.signUpHandler(false);
-        }}
-      >
-        Already have an account? Log In.
-      </p>
-    </div>
-  );
+	// }
+	return (
+		<div className={styles.loginCard}>
+			<input
+				type="email"
+				value={signUpDetails.mail}
+				placeholder="Enter mail"
+			></input>
+			<input
+				type="password"
+				value={signUpDetails.password}
+				placeholder="Enter password"
+			></input>
+			<input
+				type="password"
+				value={signUpDetails.password}
+				placeholder="Confirm password"
+			></input>
+			<button
+				className={styles.loginButton}
+				onClick={() => {
+					props.loginStateHandler(true);
+				}}
+			>
+				Sign Up
+			</button>
+			<p
+				onClick={() => {
+					props.signUpHandler(false);
+				}}
+			>
+				Already have an account? Log In.
+			</p>
+		</div>
+	);
 };
 
 export default SignUpCard;
