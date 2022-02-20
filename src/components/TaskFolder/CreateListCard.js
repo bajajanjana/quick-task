71b3744491aa22c3d7d9a9a3d41c6479e70285d9
Abhinav <thakurabhinav17122001@gmail.com>
2022-02-20 21:48:29 +0530

    :

--- src/components/TaskFolder/CreateListCard.js
+++ src/components/TaskFolder/CreateListCard.js
@@ -1,39 +1,46 @@
-import React, { useRef, useState } from 'react'
+import React, { useState } from "react";
 import * as IoIcon4 from "react-icons/io";
 import styles from "./CreateListCard.module.css";
 
 const CreateListCard = (props) => {
-  const [listCardTitle,setListCardTitle]=useState();
-  const addListCardBtnHandler=()=>{
-    props.addListCardBtnHandler (true,listCardTitle);
-    setListCardTitle('');
-  }
-  return (
-    <div>
-      <div className={styles.createListCard}>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter list title..."
-            className={styles.createListInput}
-            value={listCardTitle}
-            onChange={(e)=>{setListCardTitle(e.target.value)}}
-          ></input>
-        </div>
-        <div className={styles.addListDiv}>
-          <button className={styles.addListBtn} onClick={addListCardBtnHandler}>
-            Add list
-          </button>
-          <span>
-            <IoIcon4.IoIosClose
-              className={styles.addListCancelIcon}
-              onClick={()=>{props.addListCardBtnHandler(false,null)}}
-            ></IoIcon4.IoIosClose>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
-}
+	const [listCardTitle, setListCardTitle] = useState();
+	const addListCardBtnHandler = () => {
+		props.addListCardBtnHandler(true, listCardTitle);
+		setListCardTitle("");
+	};
+	return (
+		<div>
+			<div className={styles.createListCard}>
+				<div>
+					<input
+						type="text"
+						placeholder="Enter list title..."
+						className={styles.createListInput}
+						value={listCardTitle}
+						onChange={(e) => {
+							setListCardTitle(e.target.value);
+						}}
+					></input>
+				</div>
+				<div className={styles.addListDiv}>
+					<button
+						className={styles.addListBtn}
+						onClick={addListCardBtnHandler}
+					>
+						Add list
+					</button>
+					<span>
+						<IoIcon4.IoIosClose
+							className={styles.addListCancelIcon}
+							onClick={() => {
+								props.addListCardBtnHandler(false, null);
+							}}
+						></IoIcon4.IoIosClose>
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default CreateListCard
+export default CreateListCard;
