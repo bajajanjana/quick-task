cacda69c2d94359357109c9b95ef782dbef4c3d5
Anjana Bajaj <bajajanjana@Anjanas-MacBook-Air.local>
2022-03-20 11:27:36 +0530

    drag nd drop applied

--- src/components/TaskFolder/ListCard.js
+++ src/components/TaskFolder/ListCard.js
@@ -1,10 +1,31 @@
-import React,{useState} from "react";
+import React,{ useState, useCallback }from "react";
 import styles from "./ListCard.module.css";
 import * as IoIcon4 from "react-icons/io";
 import TaskCard from "./TaskCard";
 import CreateTaskCard from "./CreateTaskCard";
 const ListCard = (props) => {
   const [addTaskBtnActive, setAddTaskBtnActive] = useState(true);
+
+  const [tCard, setTCard] = useState(props.taskCard);
+
+  const moveTaskListItem = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragItem = tCard[dragIndex];
+      const hoverItem = tCard[hoverIndex];
+      // Swap places of dragItem and hoverItem in the pets array
+      setTCard((card) => {
+        const updatedTaskCards = [...card];
+        updatedTaskCards[dragIndex] = hoverItem;
+        updatedTaskCards[hoverIndex] = dragItem;
+
+        const updatedList = props.list.map((item) => item);
+        props.updateList(updatedList);
+
+        return updatedTaskCards;
+      });
+    },
+    [tCard]
+  );
 
   const addTaskHandler = (desc) => {
     console.log("added");
@@ -22,30 +43,52 @@ const ListCard = (props) => {
   };
   
   return (
-    <div className={styles.listCard}>
-      <h4>{props.title}</h4>
-      <hr className={styles.line}></hr>
-      <div className={styles.taskList}>
-        {props.taskCard.map((item) => {
-          return <TaskCard desc={item.desc}/>;
-        })}
+    <>
+      <div className={styles.listCard}>
+        <h4>{props.title}</h4>
+        <hr className={styles.line}></hr>
+        <div className={styles.taskList}>
+          {/* {props.taskCard.map((item, index) => {
+            return <TaskCard desc={item.desc} index={index} />;
+          })} */}
+          {props.taskCard.map((item, index) => {
+            return (
+              <TaskCard
+                desc={item.desc}
+                index={index}
+                moveListItem={moveTaskListItem}
+              />
+            );
+          })}
+        </div>
+        {addTaskBtnActive ? (
+          <button
+            className={styles.addCardBtn}
+            onClick={() => {
+              setAddTaskBtnActive(false);
+            }}
+          >
+            <span>
+              <IoIcon4.IoIosAdd className={styles.addIcon}></IoIcon4.IoIosAdd>
+            </span>
+            <span>Add a card</span>
+          </button>
+        ) : (
+          <CreateTaskCard addTaskBtnHandler={addTaskBtnHandler} />
+        )}
       </div>
-      {addTaskBtnActive ? (
-        <button
-          className={styles.addCardBtn}
-          onClick={() => {
-            setAddTaskBtnActive(false);
-          }}
-        >
-          <span>
-            <IoIcon4.IoIosAdd className={styles.addIcon}></IoIcon4.IoIosAdd>
-          </span>
-          <span>Add a card</span>
-        </button>
-      ) : (
-        <CreateTaskCard addTaskBtnHandler={addTaskBtnHandler} />
-      )}
-    </div>
+
+      {/* <div style={style}>
+        {pets.map((pet, index) => (
+          <ListItem
+            key={pet.id}
+            index={index}
+            text={pet.name}
+            moveListItem={movePetListItem}
+          />
+        ))}
+      </div> */}
+    </>
   );
 };
 
--- src/components/TaskFolder/TaskCard.js
+++ src/components/TaskFolder/TaskCard.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 import styles from "./TaskCard.module.css"
 const TaskCard = (props) => {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "item",
+    item: props.index ,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+
+  const[spec, dropRef] = useDrop({
+    accept: "item",
+    hover: (item, monitor) => {
+      const dragIndex = item.index;
+      const hoverIndex = props.index;
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const hoverActualY = monitor.getClientOffset().y - hoverBoundingRect.top;
+
+      // if dragging down, continue only when hover is smaller than middle Y
+      if (dragIndex < hoverIndex && hoverActualY < hoverMiddleY) return;
+      // if dragging up, continue only when hover is bigger than middle Y
+      if (dragIndex > hoverIndex && hoverActualY > hoverMiddleY) return;
+
+      props.moveListItem(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const ref = useRef(null);
+  const dragDropRef = dragRef(dropRef(ref));
+
+  // Make items being dragged transparent, so it's easier to see where we drop them
+  const opacity = isDragging ? 0 : 1;
   return (
-    <div className={styles.taskCard}>
-        <div>
-         {props.desc}
-        </div>
+    <div className={styles.taskCard} ref={dragDropRef}>
+      <div>{props.desc}</div>
     </div>
-  )
+  );
 }
 
 export default TaskCard
